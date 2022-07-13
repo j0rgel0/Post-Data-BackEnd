f504637dfafa680c0234f1598789538e90fe45e0
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-07-13 02:35:59 -0500

    Arreglando añadir productos al carrito desde el modal y con FETCH

--- src/main/resources/static/js/modalProductos.js
+++ src/main/resources/static/js/modalProductos.js
@@ -22,11 +22,17 @@
  
  function cargarCarrito(isbn){
     let contador2=0;
+    contador2++;
+    
     datosCarrito = JSON.parse(localStorage.getItem('elementosCarrito')) || [];
-    datos = JSON.parse(localStorage.getItem("productos")) ;
-    datosObjeto = JSON.parse(localStorage.getItem('objeto')) || [];
-    datos.forEach(element => {
-        contador2++;
+   
+   fetch('http://127.0.0.1:8087/api/products/')  
+  .then(res => res.json())  
+  .then((out) => {  
+    console.log("cargar carrito",out);  
+        
+    out.forEach(element => {
+
           if(element.isbn == isbn){
            let elemento = `{
             "img" : "${element.url_imagen}",
@@ -37,21 +43,19 @@
             "precio" : "${element.precio}",
             "id" : "${contador2}"
         }`; 
-        console.log(contador2);
-        let objeto = `{
-            "id" : "${contador2}"
-        }`; 
-        datosObjeto.forEach(element => { 
-            if(element.id == contador2){
-                console.log("Ya existe");
-            }
-        })
-        datosObjeto.push(JSON.parse(objeto));
+
         datosCarrito.push(JSON.parse(elemento));
         localStorage.setItem("elementosCarrito", JSON.stringify(datosCarrito));
-        localStorage.setItem("objeto", JSON.stringify(datosObjeto));
+        
         swal("Producto añadido correctamente", "", "success"); //SweetAlert
        
     }
-    });
+
+
+     });
+  })  
+  .catch(err => {  
+    throw err  
+  });
+ 
   }
