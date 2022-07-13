95e34916aec3794ebf235ef81e7d6b92de3de9a8
Alma <almarosalessanchez94@gmail.com>
2022-07-13 18:02:07 -0500

    se obtiene el jwt en el login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,27 +2,51 @@ let botonEnviar = document.querySelector("#botonEnviar");
 let txtEmail = document.getElementById("email");
 let txtContrasena = document.getElementById("contrasena");
 
-botonEnviar.addEventListener("click", (event)=> {
+botonEnviar.addEventListener("click", (event) => {
     event.preventDefault();
-    const validaciones=[];
-    validaciones.email=validarEmail(txtEmail.value);
-    validaciones.contraseña=validarContraseña(txtContrasena.value);
+    const validaciones = [];
+    validaciones.email = validarEmail(txtEmail.value);
+    validaciones.password = validarContraseña(txtContrasena.value);
+
+    if (validaciones.email && validaciones.password) {
+        login({ username: txtEmail.value, password: txtContrasena.value });
+    }
 
     // let usuario=localStorage.getItem("usuario")
     // console.log(usuario);
-     let usuario=JSON.parse(localStorage.getItem("usuario"));
-     const listaUsuarios=usuario.filter(usuario => usuario.email === txtEmail.value && usuario.contrasenia=== txtContrasena.value);
-    console.log(listaUsuarios);
-    
-    if (listaUsuarios[0]) {
+    //let usuario = JSON.parse(localStorage.getItem("usuario"));
+    //const listaUsuarios = usuario.filter(usuario => usuario.email === txtEmail.value && usuario.contrasenia === txtContrasena.value);
+    //console.log(listaUsuarios);
+
+    /*if (listaUsuarios[0]) {
         console.log("Chi chenol");
         document.getElementById("errorValidacion").style = 'display: none';
-        window.location.href= "index.html"
+        window.location.href = "index.html"
 
-    }else{
+    } else {
         document.getElementById("errorValidacion").style = 'display';
         document.getElementById("errorValidacion").innerHTML = "El correo o la contraseña son incorrectos.<br>";
-    }
-    // console.log(usuario2);
-
+    }*/
 });
+
+function login(usuario) {
+    fetch('http://127.0.0.1:8087/api/login/',
+        {
+            body: JSON.stringify(usuario),
+            method: 'POST',
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (!response.accesToken) {
+                swal("El correo o la contraseña son incorrectos.", "", "danger");
+            } else {
+                localStorage.setItem("token", response.accesToken);
+                document.getElementById("errorValidacion").style = 'display: none';
+                window.location.href = "index.html"        
+            }
+        })
+        .catch(error => {
+            swal("Ocurrio un error al registrar el producto", "", "danger");
+        });
+}
