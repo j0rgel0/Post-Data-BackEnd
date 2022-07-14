466959f1c9cb9deea4a0e37ca5e903a33355a199
Alma <almarosalessanchez94@gmail.com>
2022-07-13 19:05:49 -0500

    validacion token busqueda de producto

--- src/main/resources/static/js/dashboard_editar_productos.js
+++ src/main/resources/static/js/dashboard_editar_productos.js
@@ -2,9 +2,23 @@ const botonBuscar = document.querySelector("#botonBuscar");
 const txtId = document.getElementById("IdLibro");
 const itemsContainer = document.getElementById("list-items");
 
+
 botonBuscar.addEventListener("click", (event) => {
     event.preventDefault();
 
+    if (!txtId.value) {
+        swal("Debe colocar un valor correcto", "", "danger");
+    } else {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            console.log("No hay token");
+            swal("No hay token", "", "danger");
+        } else {
+            searchProduct(txtId.value, token);
+        }
+    }
+
+/*
     const cadenaProductos = localStorage.getItem('productos');
     if (cadenaProductos && cadenaProductos.length > 0) {
         const productos = JSON.parse(cadenaProductos);
@@ -23,14 +37,15 @@ botonBuscar.addEventListener("click", (event) => {
             itemsContainer.innerHTML = null;
         }
         txtId.value = null;
-    }
+    }*/
+
 });
 
 
-function addItem(item) {
+function showItem(item) {
     const itemHTML =
         `<div id="contenedorProducto" class="col-md-5 col-lg-3 col-xl-2">
-        <img id="imagenProducto" class="card-img-top p-3" src="${item.img}"/>
+        <img id="imagenProducto" class="card-img-top p-3" src="${item.url_imagen}"/>
         <div class="card-text">
             <div class="text-center">
                 <h5>${item.nombre}</h5>
@@ -43,4 +58,23 @@ function addItem(item) {
         </div>
         `;
     itemsContainer.innerHTML = itemHTML;
+}
+
+const searchProduct = (isbn, accesToken) => {
+    fetch(`http://127.0.0.1:8087/api/products/${isbn}`,
+        {
+            method: 'GET',
+            headers: { "Content-type": "application/json; charset=UTF-8", "authorization": `Bearer: ${accesToken}` }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if(response.status && response.status != 200) {
+                swal("No existe el Libro!!", "", "danger");
+            } else {
+                showItem(response);
+            }
+        })
+        .catch(error => {
+            swal("Ocurrio un error al registrar el producto", "", "danger");
+        });
 }
